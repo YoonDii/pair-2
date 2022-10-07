4827e621446ea0a75bbcaf2a78329a683bcdba59
wxxk <dwde22@gmail.com>
2022-10-07 10:30:49 +0900

    index_create_추가

---
+++ reviews/urls.py
@@ -0,0 +1,9 @@
+from django.urls import URLPattern, path
+from . import views
+
+app_name='reviews'
+
+urlpatterns=[
+    path('reviews/', views.index, name='index'),
+    path('create/', views.create, name='create'),
+] 
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'reviews/index.html')
+
+def create(request): 
+    if request.method=="POST":
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:index')
+    else:
+        review_form=ReviewForm()
+    context ={
+        "review_form": review_form
+    }
+    
+    return render(request, "reviews/create.html", context)

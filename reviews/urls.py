b6b322f84a6329d6bf36323ac54e08861172475e
w00ye0l <dnduf158@gmail.com>
2022-10-07 11:34:57 +0900

    update_delete_추가

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,6 +5,8 @@ app_name = "reviews"
 
 urlpatterns = [
     path("reviews/", views.index, name="index"),
-    path("create/", views.create, name="create"),
-    path("<int:pk>/", views.detail, name="detail"),
+    path("reviews/create/", views.create, name="create"),
+    path("reviews/<int:pk>/", views.detail, name="detail"),
+    path("reviews/<int:pk>/update/", views.update, name="update"),
+    path("reviews/<int:pk>/delete/", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -8,7 +8,9 @@ from reviews.models import Review
 
 def index(request):
     reviews = Review.objects.order_by("-id")
-    context = {"reviews": reviews}
+    context = {
+        "reviews": reviews,
+    }
     return render(request, "reviews/index.html", context)
 
 
@@ -20,12 +22,41 @@ def create(request):
             return redirect("reviews:index")
     else:
         review_form = ReviewForm()
-    context = {"review_form": review_form}
+    context = {
+        "review_form": review_form,
+    }
 
-    return render(request, "reviews/create.html", context)
+    return render(request, "reviews/form.html", context)
 
 
 def detail(request, pk):
     review = Review.objects.get(pk=pk)
-    context = {"review": review}
+    context = {
+        "review": review,
+    }
     return render(request, "reviews/detail.html", context)
+
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:index")
+    else:
+        review_form = ReviewForm(instance=review)
+    context = {
+        "review_form": review_form,
+    }
+
+    return render(request, "reviews/form.html", context)
+
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    review.delete()
+
+    return redirect("reviews:index")
